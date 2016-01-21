bb1b97a55bdf5e525cfc800630a230cff0f57f25
chensheng12330@gmail.com <chensheng12330@gmail.com>
2016-01-21 18:54:33 +0800

    Initial Commit

---
+++ SLYCameraMobDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLYCameraMobDemo
+//
+//  Created by sherwin on 16/1/21.
+//  Copyright © 2016年 SLY深蓝蕴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SLYCameraMobDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SLYCameraMobDemo
+//
+//  Created by sherwin on 16/1/21.
+//  Copyright © 2016年 SLY深蓝蕴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
